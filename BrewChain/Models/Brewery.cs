a5fc3b8981d7e52871deee80e72c3240ab2d9e8d
Rriarys <rriarys@gmail.com>
2026-04-17 11:58:00 +0300

    feat: implement initial database migration and update Brewery model

--- BrewChain/Models/Brewery.cs
+++ BrewChain/Models/Brewery.cs
@@ -2,7 +2,7 @@ namespace BrewChain.Models;
 
 public class Brewery
 {
-    public Guid Guid { get; set; }
+    public Guid Id { get; set; }
     public required string Name { get; set; }
 
     public List<Beer> Beers { get; set; } = new(); // Navigation property to represent the relationship with Beer as One-to-Many
