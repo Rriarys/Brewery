a8e16397d150c9a9631b43d929e06226c2ceb588
Rriarys <rriarys@gmail.com>
2026-04-18 14:30:18 +0300

    feat: add initial migration files for database schema including models for Beer, Brewery, Shop, and their stocks

---
+++ BrewChain/Data/Migrations/20260418112951_InitialCreate.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BrewChain.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Breweries",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Breweries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shops",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shops", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Wholesalers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Wholesalers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Beers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    AlcoholContent = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    BreweryId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Beers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Beers_Breweries_BreweryId",
+                        column: x => x.BreweryId,
+                        principalTable: "Breweries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BreweryStocks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    BreweryId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    BeerId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Quantity = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BreweryStocks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BreweryStocks_Beers_BeerId",
+                        column: x => x.BeerId,
+                        principalTable: "Beers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BreweryStocks_Breweries_BreweryId",
+                        column: x => x.BreweryId,
+                        principalTable: "Breweries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShopStocks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    ShopId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    BeerId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Quantity = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShopStocks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ShopStocks_Beers_BeerId",
+                        column: x => x.BeerId,
+                        principalTable: "Beers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ShopStocks_Shops_ShopId",
+                        column: x => x.ShopId,
+                        principalTable: "Shops",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WholesalerStocks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    WholesalerId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    BeerId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Quantity = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WholesalerStocks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WholesalerStocks_Beers_BeerId",
+                        column: x => x.BeerId,
+                        principalTable: "Beers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_WholesalerStocks_Wholesalers_WholesalerId",
+                        column: x => x.WholesalerId,
+                        principalTable: "Wholesalers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Beers_BreweryId",
+                table: "Beers",
+                column: "BreweryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BreweryStocks_BeerId",
+                table: "BreweryStocks",
+                column: "BeerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BreweryStocks_BreweryId",
+                table: "BreweryStocks",
+                column: "BreweryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShopStocks_BeerId",
+                table: "ShopStocks",
+                column: "BeerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShopStocks_ShopId",
+                table: "ShopStocks",
+                column: "ShopId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WholesalerStocks_BeerId",
+                table: "WholesalerStocks",
+                column: "BeerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WholesalerStocks_WholesalerId",
+                table: "WholesalerStocks",
+                column: "WholesalerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BreweryStocks");
+
+            migrationBuilder.DropTable(
+                name: "ShopStocks");
+
+            migrationBuilder.DropTable(
+                name: "WholesalerStocks");
+
+            migrationBuilder.DropTable(
+                name: "Shops");
+
+            migrationBuilder.DropTable(
+                name: "Beers");
+
+            migrationBuilder.DropTable(
+                name: "Wholesalers");
+
+            migrationBuilder.DropTable(
+                name: "Breweries");
+        }
+    }
+}
